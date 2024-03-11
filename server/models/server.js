460bd97e9dcdbea49cfce2b5acb3383e70ca8107
IAmTruffle <first.35580@gmail.com>
2024-03-11 14:08:09 +0700

    first commit

--- server/models/server.js
+++ server/models/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || 3099;
+    this.port = process.env.PORT || 3012;
     this.paths = {
       auth: "/api/auth",
       homepage: "/api/homepage",
